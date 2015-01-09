db0c824dee57fb6dedc6c5a0805188ca62ff11e4
Joe Dupuis <joe.dupuis@anhero.net>
2015-01-09 18:04:58 -0500

    Added patching step

--- classes/library.rb
+++ classes/library.rb
@@ -1,5 +1,5 @@
 class Library
-	attr_accessor :name, :hash, :license, :version, :archive, :url, :path, :fetch, :unpack, :build, :install, :prepareBuild, :build_subdir, :work_dir
+	attr_accessor :name, :hash, :license, :version, :archive, :url, :path, :patch, :patcher, :fetch, :unpack, :build, :install, :prepareBuild, :build_subdir, :work_dir
 	def initialize
 
 	end
