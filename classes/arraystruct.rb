718a8aad75d86e63f07e797cac713e9c87ec659e
Samuel Dionne-Riel <samuel@dionne-riel.com>
2015-01-12 14:36:53 -0500

    Wrapped the sub-elements of ArrayStruct
    This allows implicit .to_s() to call .join(' '), which is great.

--- classes/arraystruct.rb
+++ classes/arraystruct.rb
@@ -28,6 +28,41 @@ require 'ostruct'
 #    myas = ArrayStruct.new(:prop => [ 'val', 'val' ])
 #    myas.prop
 #    => [ [ 'val', 'val' ] ]
+#
+# As we have a custom use-case of those arraystructs, we need them to have a custom
+# behaviour with regards to the sub-elements.
+#
+#   * It needs to .to_s as a space separated string of the elements.
+#   * .to_s(arg) is aliased to .join(arg)
+
+class ArrayStructElement < Array
+	def initialize(*val)
+		# With a val=nil default value we would initialize with a phantom nil
+		# This gets the actual count of arguments passed.
+		return if val.length == 0
+
+		# We, though, do not support more than one argument for now...
+		raise "Not-implemented multiple values ArrayStructElement constructor" if val.length > 1
+
+		# This is why we can "hack" our way around and get the first element of the array.
+		val = val[0]
+
+		# We unpack a passed array into self<< since it is the defaut expected
+		# behaviour of an Array (I believe).
+		if val.is_a? Array then
+			val.each do |subval|
+				self << subval
+			end
+		# Otherwise, we set the value passed as the first element.
+		else
+			self << val
+		end
+	end
+
+	def to_s(arg=' ')
+		self.join(arg)
+	end
+end
 
 class ArrayStruct < OpenStruct
 
@@ -49,16 +84,13 @@ class ArrayStruct < OpenStruct
 				raise ArgumentError, "wrong number of arguments (#{len} for 1)", caller(1)
 			end
 			val = args[0]
-			#Automatically wrap in an array, unless it is an array
-			if not val.is_a?(Array) then
-				val = [val]
+			#Automatically wrap in an ArrayStrucElement, unless it is an ArrayStructElement
+			if not val.is_a?(ArrayStructElement) then
+				val = ArrayStructElement.new(val)
 			end
 			modifiable[new_ostruct_member(mname)] = val
 		elsif len == 0 && mid != :[]
-			if @table[mid].nil? then
-				@table[mid] = []
-			end
-			@table[mid]
+			self[mid]
 		else
 			raise NoMethodError, "undefined method `#{mid}' for #{self}", caller(1)
 		end
@@ -75,7 +107,7 @@ class ArrayStruct < OpenStruct
 
 	def [](name)
 		if @table[name].nil? then
-			@table[name] = []
+			@table[name] = ArrayStructElement.new()
 		end
 		@table[name]
 	end
