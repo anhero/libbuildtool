63495095a4b7136421c2f133d6c51319c6f504a0
Samuel Dionne-Riel <samuel@dionne-riel.com>
2015-01-13 15:48:49 -0500

    Documentation: Dir

--- classes/dir.rb
+++ classes/dir.rb
@@ -1,23 +1,8 @@
+# Extensions to the Ruby's +Dir+ class.
 class Dir
-	# This is an ugly function
-	# TODO: Fix to properly handle strings and hashes
-	# Man... String does have include?... though it wouldn't work properly...
-	# Furthermore, hashes' include? works with keys.
-	def self.globmask(glob, excludeMasks)
-		#I'm a noob. Don't know if there's another better way to deal with this...
-		unless excludeMasks.respond_to? "include?"
-			raise "The excludeMasks should respond to include?."
-		end
-		Dir.glob(glob).select do |file|
-			! file.split("/").any? {|part| excludeMasks.include? part}
-		end
-	end
-
-
+	# This function goes into a subdirectory when
+	# only one subdirectory is available.
 	def self.gotoSubDir
-		# This dirty function goes into a subdirectory when
-		# only one subdirectory is available.
-		#
 		# Those folders are to be ignored.
 		to_ignore = ["__MACOSX", ".", ".."]
 		listing = Dir.entries '.'
