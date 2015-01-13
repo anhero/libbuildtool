27e86f3c1d248415aebe3c3cf873bda4b69ff692
Samuel Dionne-Riel <samuel@dionne-riel.com>
2015-01-13 16:43:56 -0500

    Documentation: OS

--- classes/os.rb
+++ classes/os.rb
@@ -1,16 +1,22 @@
+# Helper to discover platform specifics.
 module OS
+
+	# @return +true+ if running platform is Windows.
 	def OS.windows?
 		(/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil
 	end
 
+	# @return +true+ if running platform is Mac OS X
 	def OS.osx?
 		(/darwin/ =~ RUBY_PLATFORM) != nil
 	end
 
+	# @return +true+ if a unix-like platform is used. (Assumes non-Windows)
 	def OS.unix?
 		!OS.windows?
 	end
 
+	# @return +true+ if platform is Linux. Wrongly assumes unix but not Mac OS X.
 	def OS.linux?
 		OS.unix? and not OS.osx?
 	end
