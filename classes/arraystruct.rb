bb93d24e1e9cd95d61a79db7c6b586c08d7ca878
Samuel Dionne-Riel <samuel@dionne-riel.com>
2015-01-13 13:00:06 -0500

    Documentation : ArrayStruct

--- classes/arraystruct.rb
+++ classes/arraystruct.rb
@@ -1,13 +1,13 @@
 require 'ostruct'
 
-# ArrayStruct
+# An ArrayStruct is basically an OpenStruct that defaults to putting 
+# all member values as arrays.
 #
-# Basically an OpenStruct that defaults to putting all member values as arrays.
-# Defining a value using '=' will replace the array.
+# Defining a value using +=+ will replace the array.
 # Appending will automagically work, even if the value is not initialized.
 #
 # When you want to build a string value from the the appended values, it is
-# recommended to .join() them with a proper separator.
+# recommended to +.join()+ them with a proper separator.
 #
 # There is one issue though. Since it does some magical wrapping to an array, when
 # you want to initialize an array of arrays, it is recommended to do
@@ -21,21 +21,113 @@ require 'ostruct'
 #    myas = ArrayStruct.new
 #    myas.prop = ['val', 'val']
 #
-# Would initialize prop as ['val', 'val'] instead of [ ['val', 'val'] ]
+# Would initialize +prop+ as +['val', 'val']+ instead of +[ ['val', 'val'] ]+ 
 #
-# On initialization (ArrayStruct.new()) with a hash, this issue would not happen.
+# On initialization +ArrayStruct.new()+ with a +Hash+, this issue would not happen.
 #
 #    myas = ArrayStruct.new(:prop => [ 'val', 'val' ])
 #    myas.prop
 #    => [ [ 'val', 'val' ] ]
 #
-# As we have a custom use-case of those arraystructs, we need them to have a custom
+# As we have a custom use-case of those +ArrayStruct+, we need them to have a custom
 # behaviour with regards to the sub-elements.
+# * It needs to +.to_s+ as a space separated string of the elements.
+# * +.to_s(arg)+ is aliased to +.join(arg)+
 #
-#   * It needs to .to_s as a space separated string of the elements.
-#   * .to_s(arg) is aliased to .join(arg)
+# This is implemented in +ArrayStruct::ArrayStructElement+
 
-class ArrayStructElement < Array
+class ArrayStruct < OpenStruct
+
+	def initialize(hash=nil)
+		@table = {}
+		if hash
+			for k,v in hash
+				@table[k.to_sym] = [ v ]
+				new_ostruct_member(k)
+			end
+		end
+	end
+
+	# Makes it possible to access members using dot notation
+	#
+	#    myas = ArrayStruct.new()
+	#    myas.prop = "bob"
+	#    p myas.prop
+	#
+	#    => "bob"
+	def method_missing(mid, *args)
+		mname = mid.id2name
+		len = args.length
+		if mname.chomp!('=') && mid != :[]=
+			if len != 1
+				raise ArgumentError, "wrong number of arguments (#{len} for 1)", caller(1)
+			end
+			val = args[0]
+			#Automatically wrap in an ArrayStrucElement, unless it is an ArrayStructElement
+			if not val.is_a?(ArrayStructElement) then
+				val = ArrayStructElement.new(val)
+			end
+			modifiable[new_ostruct_member(mname)] = val
+		elsif len == 0 && mid != :[]
+			self[mid]
+		else
+			raise NoMethodError, "undefined method `#{mid}' for #{self}", caller(1)
+		end
+	end
+
+	# Accesses a specific element as you would with a +Hash+
+	def [](name)
+		if @table[name].nil? then
+			@table[name] = ArrayStructElement.new()
+		end
+		@table[name]
+	end
+
+	protected
+	# Used internally.
+	# @private
+	def new_ostruct_member(name)
+	  name = name.to_sym
+	  unless respond_to?(name)
+		define_singleton_method(name) { @table[name] }
+		define_singleton_method("#{name}=") { |x| modifiable[name] = x }
+	  end
+	  name
+	end
+end
+
+
+# A specialization of +Array+ for semantic sugar.
+#
+# It is used mainly to allow overriding its +.to_s()+ method.
+#
+class ArrayStruct::ArrayStructElement < Array
+
+	# A new instance of ArrayStructElement.
+	#
+	# It can be initialized with nothing, giving an empty array.
+	#     ase = ArrayStructElement.new()
+	#     p ase.count
+	#     => 0
+	# 
+	# It can be initialized passing an array.
+	#     ase = ArrayStructElement.new([1,2,3])
+	#     p ase.count
+	#     => 3
+	#     p ase[1]
+	#     => 2
+	# 
+	# It can also be initialized with an initial value.
+	#     ase = ArrayStructElement.new("foo")
+	#     p ase.count
+	#     => 1
+	#     p ase[0]
+	#     => "bob"
+	#
+	# It does not mirror entirely +Array.new()+ nor does it try to exactly
+	# as it should be only used internally by +ArrayStruct+. Usage outside
+	# of +ArrayStruct+ is not recommended.
+	#
 	def initialize(*val)
 		# With a val=nil default value we would initialize with a phantom nil
 		# This gets the actual count of arguments passed.
@@ -59,56 +151,24 @@ class ArrayStructElement < Array
 		end
 	end
 
+	# Joins the elements of the array as a string, using space as a default separator.
+	#
+	# When you want to use another separator for the collection, you should
+	# use +.join(sep)+ on the collection, as it presents the intention better.
+	#     myas.PATH << "/bin"
+	#     myas.PATH << "/sbin"
+	#
+	#     p myas.PATH
+	#     => "/bin /sbin"
+	#
+	#     p myas.PATH.join(":")
+	#     => "/bin:/sbin"
+	#
+	#     # Though you could use
+	#     p myas.PATH.to_s(":")
+	#     => "/bin:/sbin"
+	#
 	def to_s(arg=' ')
 		self.join(arg)
-	end
-end
-
-class ArrayStruct < OpenStruct
-
-	def initialize(hash=nil)
-		@table = {}
-		if hash
-			for k,v in hash
-				@table[k.to_sym] = [ v ]
-				new_ostruct_member(k)
-			end
-		end
-	end
-
-	def method_missing(mid, *args) # :nodoc:
-		mname = mid.id2name
-		len = args.length
-		if mname.chomp!('=') && mid != :[]=
-			if len != 1
-				raise ArgumentError, "wrong number of arguments (#{len} for 1)", caller(1)
-			end
-			val = args[0]
-			#Automatically wrap in an ArrayStrucElement, unless it is an ArrayStructElement
-			if not val.is_a?(ArrayStructElement) then
-				val = ArrayStructElement.new(val)
-			end
-			modifiable[new_ostruct_member(mname)] = val
-		elsif len == 0 && mid != :[]
-			self[mid]
-		else
-			raise NoMethodError, "undefined method `#{mid}' for #{self}", caller(1)
-		end
-	end
-
-	def new_ostruct_member(name)
-	  name = name.to_sym
-	  unless respond_to?(name)
-		define_singleton_method(name) { @table[name] }
-		define_singleton_method("#{name}=") { |x| modifiable[name] = x }
-	  end
-	  name
-	end
-
-	def [](name)
-		if @table[name].nil? then
-			@table[name] = ArrayStructElement.new()
-		end
-		@table[name]
 	end
 end
