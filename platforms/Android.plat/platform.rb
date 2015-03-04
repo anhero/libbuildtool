44c827b05574f144cf6f01813807268b1454abec
Samuel Dionne-Riel <samuel@dionne-riel.com>
2015-03-04 16:29:30 -0500

    Updated android platforms for our latest changes.
    It broke.

--- platforms/Android.plat/platform.rb
+++ platforms/Android.plat/platform.rb
@@ -1,12 +1,12 @@
+require 'pp'
 module Platforms
 	class Android < Platform
-
-
 		def initialize
-			@build_options = ArrayStruct.new(:configure_options => '--host=arm-android-linux', :arch => 'arm')
-			$options.install_dir = "#{Dir.pwd}/Android/#{@build_options.arch}"
-
-			$options.compiler if $options.compiler == nil
+			# Defaults
+			@build_options = ArrayStruct.new(:configure_options => '--host=arm-android-linux')
+			@build_options.arch = "arm"
+			$libbuildtool_params.install_dir = "#{Dir.pwd}/Android/#{@build_options.arch.first}"
+			$libbuildtool_params.compiler if $libbuildtool_params.compiler == nil
 			@optionParser = OptionParser.new do |opts|
 				opts.on('-a', '--arch', '=ARCHITECTURE', 'Architecture', "Default: arm") do |arch|
 					@build_options.arch = arch
@@ -15,7 +15,7 @@ module Platforms
 					elsif arch == 'x86'
 						@build_options.configure_options = '--host=x86-android-linux'
 					end
-					$options.install_dir = "#{Dir.pwd}/Android/#{@build_options.arch}"
+					$libbuildtool_params.install_dir = "#{Dir.pwd}/Android/#{@build_options.arch.first}"
 				end
 			end
 		end
@@ -23,15 +23,6 @@ module Platforms
 		def permute!
 			@optionParser.permute!
 		end
-
-
-
-=begin
-		def self.short_desc
-			return 'Cygwin distribution of the Mingw compiler. Used to compile windows native trough Cygwin (Not for MSYS).'
-		end
-=end
-
 
 	end
 
